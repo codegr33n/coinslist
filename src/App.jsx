aa2405dbd9168d0778ab0ed90a2642d0a17a992b
codegr33n <greenbrightsmart@gmail.com>
2023-06-24 13:28:47 +0100

    fetched coin markets from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+
+import axios from "axios";
+import Coins from "./components/Coins";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [coins, setCoins] = useState([]);
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en";
+
+  // API call
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data[0]);
+      })
+      .catch((err) => console.log(err.message));
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Coins coins={coins} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
